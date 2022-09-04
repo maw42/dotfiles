a91b2916b3c3d27f8da2ebd683e2e6324262241e
MaW <info@maw42.de>
2022-09-04 19:17:00 +0200

    Add final Emacs cfg (for now)

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -17,17 +17,13 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
-;; color-theme. zenburn, monokai, spacemacs and solarized are best-supported regarding syntax-hl
-(use-package spacemacs-dark-theme
-  :config (load-theme 'spacemacs-dark t))
-;; shows possible key commands after typing
-(require 'guide-key)
-(setq guide-key/guide-key-sequence t)
-(guide-key-mode 1)
+
 
 ;; needs package org-modern
 (global-org-modern-mode)
 ;; olivetti-mode is a good package idea on top
+;; set directory for org-mode attachments
+(setq org-attach-id-dir "~/Documents/org-pkm/assets")
 
 (use-package org-roam
 	     :ensure t
@@ -40,7 +36,29 @@
 		    ("C-c n i" . org-roam-node-insert))
 	     :config
 	     (org-roam-setup))
+;; exclude all headlines wiht attachments from org-roam caching
+(setq org-roam-db-node-include-function
+      (lambda ()
+        (not (member "ATTACH" (org-get-tags)))))
+
+
+;; org-templates
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+olp "~/Documents/pkm/foo.org" "capture:" "Tasks:")
+         "* TODO %?\n  %i")
+	("n" "short note" entry (file+olp "~/Documents/pkm/foo.org" "capture:" )
+	 "* NOTE %?\n")
+        ("m" "Meeting" entry (file+olp "~/Documents/pkm/foo.org" "capture:" "Meetings:")
+         (file "~/Documents/pkm/templates/tpl-meeting.org"))
+	("j" "Journal entry" plain (function org-journal-find-location)
+                               "** %(format-time-string org-journal-time-format)%^{Title}\n%i%?"
+                               :jump-to-captured t :immediate-finish t)
+	)
+)
+(setq org-agenda-files (quote ("~/Documents/pkm/" "~/Documents/pkm/journals")))
+
 (setq org-hide-emphasis-markers t)
+
 ;; the org-journal-dir has to be set before loading org-journal (see https://www.emacswiki.org/emacs/OrgJournal)
 (setq org-journal-dir "~/Documents/org-pkm/journals/")
 (setq org-journal-file-format "%Y-%m-%d")
@@ -55,14 +73,29 @@
     (org-narrow-to-subtree))
   (goto-char (point-max)))
 
-(setq org-capture-templates '(("j" "Journal entry" plain (function org-journal-find-location)
-                               "** %(format-time-string org-journal-time-format)%^{Title}\n%i%?"
-                               :jump-to-captured t :immediate-finish t)))
-;; enable org-roam at startup and start caching (takes forever)
-;;(org-roam-db-autosync-mode)
+;; set variable height for font-faces
+;; (let* ((variable-tuple
+;;           (cond ((x-list-fonts "ETBembo")         '(:font "ETBembo"))
+;;                 ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+;;                 ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
+;;                 ((x-list-fonts "Verdana")         '(:font "Verdana"))
+;;                 ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
+;;                 (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+;;          (base-font-color     (face-foreground 'default nil 'default))
+;;          (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
 
-;; week starts on Monday
-(setq calendar-week-start-day 1)
+;;     (custom-theme-set-faces
+;;      'user
+;;      `(org-level-8 ((t (,@headline ,@variable-tuple))))
+;;      `(org-level-7 ((t (,@headline ,@variable-tuple))))
+;;      `(org-level-6 ((t (,@headline ,@variable-tuple))))
+;;      `(org-level-5 ((t (,@headline ,@variable-tuple))))
+;;      `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
+;;      `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
+;;      `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
+;;      `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
+;;      `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
+
 ;; TODO midterm: replace Selectrum by Helm or Ivy
 ;; Selectrum -- Completion in minibuffer
 (selectrum-mode +1)
@@ -72,15 +105,33 @@
 ;; intelligent over time
 (prescient-persist-mode +1)
 
-(global-set-key (kbd "C-c a") 'org-agenda)
+
+;; color-theme. zenburn, monokai, spacemacs and solarized are best-supported regarding syntax-hl
+(use-package spacemacs-dark-theme
+  :config (load-theme 'spacemacs-dark t))
+;; week starts on Monday, not Sunday
+(setq calendar-week-start-day 1)
+;; shows possible key commands after typing
+(require 'guide-key)
+(setq guide-key/guide-key-sequence t)
+(guide-key-mode 1)
+
+;;
+;; user-keys
+;;
+
+(global-set-key "\C-c a" 'org-agenda)
 (global-set-key (kbd "<f6>") 'org-capture)
+
+;;
+;; custom-GUI-cfgs
+;;
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   '("/home/maw/Documents/org-pkm/20220824173402-programming_languages.org" "/home/maw/Documents/org-pkm/20220824174829-emacs_lisp.org" "/home/maw/Documents/org-pkm/20220824175119-scheme.org" "/home/maw/Documents/org-pkm/20220824175721-schedule.org" "/home/maw/Documents/org-pkm/20220824180753-system_crafters.org" "/home/maw/Documents/org-pkm/done.org" "/home/maw/Documents/org-pkm/gtd.org" "/home/maw/Documents/org-pkm/journals/2022-08-28"))
  '(org-log-done 'time)
  '(org-log-into-drawer t)
  '(org-log-reschedule 'note)
@@ -88,7 +139,7 @@
  '(org-refile-use-outline-path t)
  '(org-roam-completion-everywhere t)
  '(package-selected-packages
-   '(org-journal spacemacs-theme zenburn-theme guide-key monokai-pro-theme selectrum-prescient selectrum org-roam))
+   '(evil magit org-journal spacemacs-theme zenburn-theme guide-key monokai-pro-theme selectrum-prescient selectrum org-roam))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
